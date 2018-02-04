0b929c2f77e4befbe94580b227c6f843bd1140ae
Steve <sel@users.noreply.github.com>
2018-02-04 10:29:36 +0100

    Add Apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,51 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://127.0.0.1:4000/graphql' }),
+  cache: new InMemoryCache()
+});
+
+const deploymentListQuery = gql`
+  query DeploymentListQuery {
+    deployments {
+      id
+      name
+    }
+  }
+`;
+
+const DeploymentList = ({ data: { loading, error, deployments } }) => {
+  if (loading) {
+    return <p>Loading ...</p>;
+  }
+  if (error) {
+    return <p>{error.message}</p>;
+  }
+  return <ul>{deployments.map(d => <li key={d.id}>{d.name}</li>)}</ul>;
+};
+
+const DeploymentListWithData = graphql(deploymentListQuery)(DeploymentList);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Kubernetes Deployments</h1>
+          </header>
+          <DeploymentListWithData />
+        </div>
+      </ApolloProvider>
     );
   }
 }
