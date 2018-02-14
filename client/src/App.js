b973c5af350e4a5f229ba6a92f1fc06ea13b1be8
Steve <sel@users.noreply.github.com>
2018-02-14 21:07:49 +0100

    client: Factor-out DeploymentList component

--- client/src/App.js
+++ client/src/App.js
@@ -5,34 +5,13 @@ import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { graphql } from 'react-apollo';
-import gql from 'graphql-tag';
+
+import DeploymentListWithData from './components/DeploymentList';
 
 const client = new ApolloClient({
   link: new HttpLink({ uri: 'http://127.0.0.1:4000/graphql' }),
   cache: new InMemoryCache()
 });
-
-const deploymentListQuery = gql`
-  query DeploymentListQuery {
-    deployments {
-      id
-      name
-    }
-  }
-`;
-
-const DeploymentList = ({ data: { loading, error, deployments } }) => {
-  if (loading) {
-    return <p>Loading ...</p>;
-  }
-  if (error) {
-    return <p>{error.message}</p>;
-  }
-  return <ul>{deployments.map(d => <li key={d.id}>{d.name}</li>)}</ul>;
-};
-
-const DeploymentListWithData = graphql(deploymentListQuery)(DeploymentList);
 
 class App extends Component {
   render() {
---
+++ client/src/components/DeploymentList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const deploymentListQuery = gql`
+  query DeploymentListQuery {
+    deployments {
+      id
+      name
+    }
+  }
+`;
+
+const DeploymentList = ({ data: { loading, error, deployments } }) => {
+  if (loading) {
+    return <p>Loading ...</p>;
+  }
+  if (error) {
+    return <p>{error.message}</p>;
+  }
+  return <ul>{deployments.map(d => <li key={d.id}>{d.name}</li>)}</ul>;
+};
+
+export default graphql(deploymentListQuery)(DeploymentList);
